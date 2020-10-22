105484293f84900bcb5f6f3f0a42f7a8ba176ab0
znguyen <znguyen@getraline.com>
2020-10-22 10:25:47 +0200

    admin panel - easy admin

---
+++ src/Controller/Admin/BlogPostCrudController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\BlogPost;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+
+class BlogPostCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return BlogPost::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('title'),
+            DateTimeField::new('published'),
+            TextEditorField::new('content'),
+            TextField::new('author'),
+            TextField::new('slug')
+        ];
+    }
+    
+}
